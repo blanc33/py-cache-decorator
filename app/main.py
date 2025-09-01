d53defebb71cac74603602b212b6c20148636ed8
Liubov Kobylianska <blondblanc3@gmail.com>
2025-09-01 10:23:28 -0500

    Solution5

--- app/main.py
+++ app/main.py
@@ -1,22 +1,22 @@
 from typing import Callable
 from functools import wraps
 
+
 def cache(func: Callable) -> Callable:
-        history = {}
+    history = {}
 
-        @wraps(func)
-        def wrapper(*args, **kwargs) -> tuple:
-            kw_parameters = tuple(kwargs.items())
-            func_id = (func.__name__, args, kw_parameters)
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> tuple:
+        kw_parameters = tuple(kwargs.items())
+        func_id = (func.__name__, args, kw_parameters)
 
-            if func_id in history:
-                print("Getting from cache")
-                return history[func_id]
-            else:
-                print("Calculating new result")
-                result = func(*args, **kwargs)
-                history[func_id] = result
-                return result
+        if func_id in history:
+            print("Getting from cache")
+            return history[func_id]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            history[func_id] = result
+            return result
 
-        return wrapper
-
+    return wrapper
