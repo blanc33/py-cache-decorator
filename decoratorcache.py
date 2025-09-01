0315527bd608708a87ac7f7b8f7b8aa7f0352b9c
Liubov Kobylianska <blondblanc3@gmail.com>
2025-09-01 10:08:04 -0500

    Solution

--- decoratorcache.py
+++ decoratorcache.py
@@ -1,19 +1,17 @@
-def cache(func, history={}):
-    def wrapper(*args):
-        if args in history:
+from functools import wraps
+
+def cache(func: callable) -> callable:
+    history = {}
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> tuple:
+        kw_parameters = tuple(kwargs.items())
+        func_id = (func.__name__, args, kw_parameters)
+        if func_id in history:
             print("Getting from cache")
-            return history[args]
+            return history[func_id]
         else:
             print("Calculating new result")
-            result = func(*args)
-            history[args] = result
+            result = func(*args, **kwargs)
+            history[func_id] = result
             return result
     return wrapper
-
-@cache
-def long_time_func(a: int, b: int, c: int) -> int:
-    return (a ** b ** c) % (a * c)
-
-@cache
-def long_time_func_2(n_tuple: tuple, power: int) -> int:
-    return [number ** power for number in n_tuple]
